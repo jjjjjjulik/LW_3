65fe193c328bdc5ff762906137be8460712bc2c0
jjjjjjulik <julgab2309@gmail.com>
2023-04-13 12:17:06 +0300

    Опис змін

--- DAL/AnnouncementRepository.cs
+++ DAL/AnnouncementRepository.cs
@@ -13,7 +13,7 @@ namespace DAL
         public AnnouncementRepository(MyProjectDbContext context) : base(context)
         {
         }
-
-        // Тут можна додати специфічні методи для AnnouncementRepository
     }
+
+       // Тут можна додати специфічні методи для AnnouncementRepository
 }
