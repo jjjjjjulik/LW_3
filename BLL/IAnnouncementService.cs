1345b144e7d92d7110358b27fefd79447675f405
jjjjjjulik <julgab2309@gmail.com>
2023-04-17 20:41:49 +0300

    Initial commit

--- BLL/IAnnouncementService.cs
+++ BLL/IAnnouncementService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace BLL
 {
-    internal interface IAnnouncementService
+    public interface IAnnouncementService
     {
+        IEnumerable<Announcement> GetAllAnnouncements();
+        Announcement GetAnnouncementById(int id);
+        void AddAnnouncement(Announcement announcement);
+        void UpdateAnnouncement(Announcement announcement);
+        void DeleteAnnouncement(int id);
     }
 }
